fa588d4420bfcbeeb845df31ab82d4a3eef89e19
zoscarcito <65929424+zoscarcito@users.noreply.github.com>
2022-01-18 23:10:24 -0500

    Add files via upload

---
+++ js/105029386685.js
@@ -0,0 +1,14 @@
+window.mcwidget = {"appId":"532160876956612","pageId":"105029386685","page_name":"Belize Vacation","fb_page_id":"105029386685","widget_phone":null,"sms_channel_connected":false,"widgets":[{"widget_id":16323621,"page_id":"105029386685","widget_type":"customer_chat","status":"active","name":"Growth Tool #1","data":[],"channel":"facebook","ts_created":1639428107,"chat_ref":"w16323621"}],"widgetLocale":"en_US","defaultSize":500,"fbSDKVersion":"v12.0","whitelist":["e04a45f0a505569fddc714decd22a84e8cb421b628b371dd70f2ca7035eac346","f2cbfeda670dc91d10651057f2d9c1f9010175cf28059cfc43337b7d2c6f3f4d","36d89a1ba8cf52036f81a963c230938b18519267ee2a0e7203de79da8f03be17","264653555c98a533392b71bec82c2c8f16c4f0336777a11a36053df09a533397","ff95628df845f578f173f84015b67d081a461af2ef3a50a47fc78c022041e2c1","bf76dfe99073a8bea601af504fd300ff4e3dcda349560867ead969ff4c691181","ccb5a176920b1b24aaa064547dd684c5600e84e7ea0b7d93d7dc55ae60683146"],"smsModalConsentText":"Please note that by typing in your phone number in the field above you are agreeing to receive autodialed personal and marketing text messages to your mobile number. Consent is not required for purchase. Message and data rates may apply. Reply “STOP” by SMS to cancel."};
+
+
+    (function(d, s, id){
+        var host = 'mccdn.me';
+
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = '//' + host + '/assets/js/widget.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'mcwidget-core'));
+
+            
---
+++ js/app_banner_config.js
@@ -0,0 +1,14 @@
+var vwAppBannerSettings = {
+  appleAppId: '1566428824',
+  googleAppId: 'com.visitwidget.travelbelize',
+  minutesToKeepBannerClosed: null,
+  showOnDesktop: false,
+  showOnIOs: true,
+  showOnAndroid: true,
+  appIcon: 'https://dfht7c9lgb1wh.cloudfront.net/clients/app_icons/000/000/699/original/icon-114x114-1625235186.png?1625235186',
+  title: 'Travel Belize',
+  author: 'Visit Widget LLC',
+  analyticsTrackingId: 'UA-113166354-11',
+  onShow: function() {  },
+  onHide: function() {  }
+}
